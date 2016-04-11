90190638269030160b3150f52057740ca6601186
Nerfertius <nerfertius@gmail.com>
2016-04-12 00:31:22 +0200

    Removed old builds & fixed movie switch
    Removed old builds
    Added script for switching movie
    Switchig movie also switches audio

--- Hemtenta simulator/Assets/Script/MovieScript.cs
+++ Hemtenta simulator/Assets/Script/MovieScript.cs
@@ -9,21 +9,33 @@ public class MovieScript : MonoBehaviour
 
 	private MovieTexture m_texture;
 	private Renderer r;
+	private AudioSource a_source;
 
 	void Start ()
     {
-		m_texture = StartMovie;
 		r = GetComponent<Renderer>();
-		//r.material.SetTexture(m_texture.ToString(), m_texture);
+		m_texture = StartMovie;
+		r.material.mainTexture = m_texture;
+		a_source = gameObject.GetComponent<AudioSource>();
 	}
 	
 	void Update ()
     {
-        //if (m_texture.isPlaying == false)
-		//{
-			m_texture = movies[Random.Range(0, movies.Count - 1)];
-			r.material.SetTexture(m_texture.ToString(), m_texture);
-			m_texture.Play();
-		//}
+		if (m_texture.isPlaying == false)
+		{
+			//r.material.mainTexture = StartMovie;
+			//a_source.clip = m_texture.audioClip;
+			//a_source.Play();
+			//StartMovie.Play();
+
+			//if (StartMovie.isPlaying == false)
+			//{
+				m_texture = movies[Random.Range(0, movies.Count - 1)];
+				r.material.mainTexture = m_texture;
+				a_source.clip = m_texture.audioClip;
+				a_source.Play();
+				m_texture.Play();
+			//}
+		}
     }
 }
