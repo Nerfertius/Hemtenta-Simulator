e3e769eaa50a8db0cf501bbcbe4e551aff8cd617
Nerfertius <nerfertius@gmail.com>
2016-04-16 22:36:36 +0200

    MovieScript is now finished and working
    Moviescript doesn't play the same video twice. in a row.
    A new game version has started, where the player has three lives and
    loses one every time they look away from the test.

--- Hemtenta simulator/Assets/Scripts/MovieScript.cs
+++ Hemtenta simulator/Assets/Scripts/MovieScript.cs
@@ -10,6 +10,7 @@ public class MovieScript : MonoBehaviour
 	private MovieTexture m_texture;
 	private Renderer r;
 	private AudioSource a_source;
+	private MovieTexture PreviousMovie;
 
 	void Start ()
     {
@@ -37,18 +38,26 @@ public class MovieScript : MonoBehaviour
 	{
 		m_texture = movies[Random.Range(0, movies.Count - 1)];
 		Debug.Log(m_texture);
+		Debug.Log(PreviousMovie);
+
+		if (m_texture == PreviousMovie)
+		{
+			m_texture = movies[Random.Range(0, movies.Count - 1)];
+			PreviousMovie = m_texture;
+		}
+		PreviousMovie = m_texture;
 		r.material.mainTexture = m_texture;
 		a_source.clip = m_texture.audioClip;
 		m_texture.Stop();
 		a_source.Play();
 		m_texture.Play();
-		TimeBetweenClips(m_texture.duration);
+		StartCoroutine(TimeBetweenClips());
 	}
 
-	IEnumerator TimeBetweenClips(float time)
+	IEnumerator TimeBetweenClips()
 	{
 		Debug.Log("Waiting...");
-		yield return new WaitForSeconds(time);
+		yield return new WaitForSeconds(m_texture.duration + 0.5F);
 		Debug.Log("Going");
 		NewClip();
 	}
